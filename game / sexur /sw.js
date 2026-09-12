adae4938ae7648ecb8c4b32642dad57a24de6c22
dsefwevsdw <soti03e250058@epu.edu.iq>
2026-09-12 06:02:58 +0300

    Add service worker for file caching
    Implement service worker for caching files.

---
+++ game / sexur /sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'sexur-v2';
+const FILES = [
+  './',
+  './index.html',
+  './sexur.webp',
+  './bsexur.webp',
+  './ads.webp'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
